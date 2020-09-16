2ed645a48a6e897b48ba0ae2624d2208a215981f
Lyoung0789 <Lestermyoung07@gmail.com>
2020-09-16 16:28:45 -0400

    Adding seperate component for each item, and the onPress event to remove the item

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View, FlatList } from "react-native";
 import Header from "./components/header";
+import TodoItem from "./components/todoItem";
 
 export default function App() {
   const [todos, setTodos] = useState([
-    { text: "buy coffee", key: 1 },
-    { text: "buy deodarant", key: 2 },
-    { text: "Laundry", key: 3 },
-    { text: "Learn React Native!", key: 4 },
+    { text: "buy coffee", key: "1" },
+    { text: "buy deodarant", key: "2" },
+    { text: "Laundry", key: "3" },
+    { text: "Learn React Native!", key: "4" },
   ]);
+
+  const pressHandler = (key) => {
+    console.log(key);
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.key != key);
+    });
+  };
 
   return (
     <View style={styles.container}>
@@ -18,7 +26,9 @@ export default function App() {
         <View style={styles.list}>
           <FlatList
             data={todos}
-            renderItem={({ item }) => <Text>{item.text}</Text>}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler} />
+            )}
           />
         </View>
       </View>
