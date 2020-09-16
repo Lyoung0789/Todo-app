49a2b7b02dcf25599638dd95273a7f8b2e8b2d52
Lyoung0789 <Lestermyoung07@gmail.com>
2020-09-16 17:10:00 -0400

    added some validations, todo needs to be atleast 3 chars long

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import { StyleSheet, Text, View, FlatList, Alert } from "react-native";
 import Header from "./components/header";
 import TodoItem from "./components/todoItem";
 import AddTodo from "./components/addTodo";
@@ -20,9 +20,18 @@ export default function App() {
   };
 
   const pressButton = (text) => {
-    setTodos((prevTodos) => {
-      return [{ text: text, key: Math.random().toString() }, ...prevTodos];
-    });
+    if (text.length > 3) {
+      setTodos((prevTodos) => {
+        return [{ text: text, key: Math.random().toString() }, ...prevTodos];
+      });
+    } else {
+      Alert.alert("Oops", "Todos must be over 3 chars long", [
+        {
+          text: "Understood",
+          onPress: () => console.log("alert closed"),
+        },
+      ]);
+    }
   };
 
   return (
